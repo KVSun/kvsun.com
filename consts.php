8a236f641668cbbfc818cee1f644c216d563b53e
Chris Zuber <shgysk8zer0@gmail.com>
2016-11-10 11:18:55 -0800

    Add various resource files
    Names should be descriptive enough

---
+++ consts.php
@@ -0,0 +1,6 @@
+<?php
+namespace KVSun;
+const EXT        = '.php';
+const COMPONENTS = __DIR__ . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR;
+const CLASSES    = __DIR__ . DIRECTORY_SEPARATOR . 'classes';
+const CONFIG     = __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
