2a584ac50a091f0af8e4480156720257a549cd92
Chris Zuber <shgysk8zer0@gmail.com>
2016-11-13 12:42:13 -0800

    Add form handlers
    - In `api.php`, check for form in `$_REQUEST` and load form handler
    - Create handler for installer

--- api.php
+++ api.php
@@ -2,6 +2,7 @@
 namespace KVSun;
 
 use \shgysk8zer0\Core as Core;
+use \shgysk8zer0\Core_API\Abstracts\HTTPStatusCodes as Status;
 
 ob_start();
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
@@ -12,14 +13,23 @@ if (DEBUG) {
 
 $header = Core\Headers::getInstance();
 if ($header->accept === 'application/json') {
-	$resp = new Core\JSON_Response();
-	$resp->notify('Title', $_REQUEST['url'], DOMAIN . 'images/sun-icons/128.png');
-	$resp->send();
+	if (array_key_exists('form', $_REQUEST) and is_array($_REQUEST[$_REQUEST['form']])) {
+		require_once COMPONENTS . 'handlers' . DIRECTORY_SEPARATOR . 'form.php';
+	} else {
+		$resp->notify('Invalid request', null, DOMAIN . 'images/sun-icons/128.png');
+	}
+	Core\JSON_Response::getInstance()->send();
+	exit();
 } elseif(array_key_exists('url', $_GET)) {
-	$header->location = $_GET['url'];
-	http_response_code(303);
+	$url = new Core\URL($_GET['url']);
+	if ($url->host === $_SERVER['SERVER_NAME']) {
+		$header->location = "{$url}";
+		http_response_code(Status\SEE_OTHER);
+	} else {
+		http_response_code(Status\BAD_REQUEST);
+	}
 	exit();
 } else {
-	http_response_code(404);
+	http_response_code(Status\BAD_REQUEST);
 	exit();
 }
---
+++ components/handlers/form.php
@@ -0,0 +1,57 @@
+<?php
+namespace KVSun\Components\Handlers\Form;
+
+use \shgysk8zer0\Core as Core;
+use \shgysk8zer0\DOM as DOM;
+use \shgysk8zer0\Core_API as API;
+use \shgysk8zer0\Core_API\Abstracts\HTTPStatusCodes as Status;
+
+$resp = Core\JSON_Response::getInstance();
+
+switch($_REQUEST['form']) {
+	case 'install-form':
+		if (!is_dir(\KVSun\CONFIG)) {
+			$resp->notify('Config dir does not exist', \KVSun\CONFIG);
+		} elseif (! is_writable(\KVSun\CONFIG)) {
+			$resp->notify('Cannot write to config directory', \KVSun\CONFIG);
+		} elseif (file_exists(\KVSun\CONFIG . \KVSun\DB_CREDS)) {
+			$resp->notify('Already installed', 'Database config file exists.');
+		} elseif (! file_exists(\KVSun\DB_INSTALLER)) {
+			$resp->notify('SQL file not found', 'Please restore "default.sql" using Git.');
+		} else {
+			$installer = $_POST['install-form'];
+			Core\Console::getInstance()->log($installer['db']);
+			if (array_key_exists('db', $installer) and is_array($installer['db'])) {
+				try {
+					file_put_contents(
+						\KVSun\CONFIG . \KVSun\DB_CREDS,
+						json_encode($installer['db'], JSON_PRETTY_PRINT)
+					);
+					$pdo = new Core\PDO();
+					if ($pdo->connected) {
+						if (empty($pdo->showTables())) {
+							if (! $pdo->restore(\KVSun\DB_INSTALLER)) {
+								$resp->notify('Error', 'Could not restore database.');
+							} else {
+								$resp->notify('Installed', 'Created new default database.');
+								$resp->reload();
+							}
+						} else {
+							$resp->notify('Installed', 'Using existing database.');
+							$resp->reload();
+						}
+					} else {
+						$resp->notify('Error', 'Could not connect to database using given credentials.');
+						unlink(\KVSun\DB_CREDS);
+					}
+				} catch(\Exception $e) {
+					$resp->notify('Error', $e->getMessage());
+				}
+			} else {
+				$resp->notify('Missing input', 'Please fill out the form correctly.');
+			}
+		}
+		break;
+	default:
+		http_response_code(Status::BAD_REQUEST);
+}
