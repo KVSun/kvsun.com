4f443026df8bcad72503ba0caf8513cdeac9fa7d
Chris Zuber <shgysk8zer0@gmail.com>
2016-11-18 13:01:47 -0800

    Update various handlers
    Will give better description when it's for more than testing

--- api.php
+++ api.php
@@ -13,14 +13,29 @@ if (DEBUG) {
 
 $header = Core\Headers::getInstance();
 if ($header->accept === 'application/json') {
-	if (array_key_exists('form', $_REQUEST) and is_array($_REQUEST[$_REQUEST['form']])) {
+	$resp = Core\JSON_Response::getInstance();
+	if (array_key_exists('url', $_GET)) {
+		$url = new Core\URL($_GET['url']);
+		$page = new \KVSun\Page($url);
+		$header->content_type = 'application/json';
+		Core\Console::getInstance()->log($page);
+		exit($page);
+	} elseif (array_key_exists('form', $_REQUEST) and is_array($_REQUEST[$_REQUEST['form']])) {
 		require_once COMPONENTS . 'handlers' . DIRECTORY_SEPARATOR . 'form.php';
+	} elseif (array_key_exists('datalist', $_GET)) {
+		$resp->notify('Request for datalist', $_GET['datalist']);
+	} elseif (array_key_exists('load_menu', $_GET)) {
+		if (@file_exists(COMPONENTS . $_GET['load_menu'] . '.html')) {
+			$resp->append('body', file_get_contents(COMPONENTS . $_GET['load_menu'] . '.html'));
+		} else {
+			$resp->notify('Request for menu', $_GET['load_menu']);
+		}
 	} else {
 		$resp->notify('Invalid request', null, DOMAIN . 'images/sun-icons/128.png');
 	}
-	Core\JSON_Response::getInstance()->send();
+	$resp->send();
 	exit();
-} elseif(array_key_exists('url', $_GET)) {
+}  elseif(array_key_exists('url', $_GET)) {
 	$url = new Core\URL($_GET['url']);
 	if ($url->host === $_SERVER['SERVER_NAME']) {
 		$header->location = "{$url}";
@@ -28,8 +43,8 @@ if ($header->accept === 'application/json') {
 	} else {
 		http_response_code(Status\BAD_REQUEST);
 	}
-	exit();
-} else {
+}  else {
 	http_response_code(Status\BAD_REQUEST);
-	exit();
+	$header->content_type = 'application/json';
+	exit('{}');
 }
