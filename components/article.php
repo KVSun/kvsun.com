6d3f556e07aa91eb68f88bff6f4548706c62ae29
Chris Zuber <shgysk8zer0@gmail.com>
2016-11-18 16:25:20 -0800

    Check for valid article contents
    Trigger an error if `$page` is not correct.

--- components/article.php
+++ components/article.php
@@ -3,9 +3,13 @@ namespace KVSun\Components\Article;
 return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page)
 {
 	$article = $dom->getElementsByTagName('main')->item(0)->append('article');
-	$header = $article->append('header');
-	$header->append('h2', $page->title);
-	$header->append('span', "By {$page->author}");
-	$header->append('span', " on {$page->posted}");
-	$article->importHTML($page->content);
+	if (is_object($page) and isset($page->content, $page->posted, $page->title)) {
+		$header = $article->append('header');
+		$header->append('h2', $page->title);
+		$header->append('span', "By {$page->author}");
+		$header->append('span', " on {$page->posted}");
+		$article->importHTML($page->content);
+	} else {
+		trigger_error('Invalid page contents given.');
+	}
 };
