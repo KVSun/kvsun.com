71b87dcd537c4683147c18ab9fb1a767409a7d60
Chris Zuber <shgysk8zer0@gmail.com>
2016-12-09 14:25:55 -0800

    Set include path according to `INCLUDE_PATH`

--- autoloader.php
+++ autoloader.php
@@ -3,5 +3,16 @@ namespace KVSun;
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'consts.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 
-set_include_path(CLASSES . PATH_SEPARATOR . CONFIG . PATH_SEPARATOR . get_include_path());
+set_path(INCLUDE_PATH);
 spl_autoload_register('spl_autoload');
+
+
+function set_path(Array $path, $use_existing = true)
+{
+	$path = array_map('realpath', $path);
+	$path = join(\PATH_SEPARATOR, $path);
+	if ($use_existing === true) {
+		$path .= \PATH_SEPARATOR . get_include_path();
+	}
+	return set_include_path($path);
+}
