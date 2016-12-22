8702e99aa6862c7cc6dd545ef8638b0adff744ce
Chris Zuber <shgysk8zer0@gmail.com>
2016-12-22 11:33:01 -0800

    Whitespace fix

--- consts.php
+++ consts.php
@@ -5,7 +5,7 @@ const INCLUDE_PATH = array(
 	'./classes',
 	'./config',
 	__DIR__,
-	);
+);
 const COMPONENTS   = __DIR__ . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR;
 // const CLASSES      = __DIR__ . DIRECTORY_SEPARATOR . 'classes';
 const CONFIG       = __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
