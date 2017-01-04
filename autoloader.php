0fa28b5b15a48f69da9859dc56ffe224a33c8df4
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-04 12:44:18 -0800

    Start session in autoloader

--- autoloader.php
+++ autoloader.php
@@ -6,6 +6,9 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 set_path(INCLUDE_PATH);
 spl_autoload_register('spl_autoload');
 
+session_name($_SERVER['SERVER_NAME']);
+session_set_cookie_params(0, '/', $_SERVER['HTTP_HOST'], array_key_exists('HTTPS', $_SERVER), true);
+session_start();
 
 function set_path(Array $path, $use_existing = true)
 {
