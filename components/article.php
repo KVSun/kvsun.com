dbd7064667853a0323de805044095187634f7d52
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-06 07:47:51 -0800

    Update posted time in articles
    - Uses `<time>`
    - Formats data in a friendlier form
    - Adds `datetime` attribute

--- components/article.php
+++ components/article.php
@@ -4,10 +4,17 @@ return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page)
 {
 	$article = $dom->getElementsByTagName('main')->item(0)->append('article');
 	if (is_object($page) and isset($page->content, $page->posted, $page->title)) {
+		$posted = new \DateTime($page->posted);
 		$header = $article->append('header');
 		$header->append('h2', $page->title);
 		$header->append('span', "By {$page->author}");
-		$header->append('span', " on {$page->posted}");
+		$header->append(
+			'time',
+			" on {$posted->format('D. M j, Y \a\t h:m a')}",
+			[
+				'datetime' => $posted->format('Y-m-d\TH:m'),
+			]
+		);
 		$article->importHTML($page->content);
 	} else {
 		trigger_error('Invalid page contents given.');
