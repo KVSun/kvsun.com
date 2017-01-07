676a159b30b39c4188d9cbd597eed998fd8fc700
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-07 12:30:30 -0800

    Remove extra whitespace

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ if (version_compare(PHP_VERSION, '5.6', '<')) {
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 
-
 DOM\HTMLElement::$import_path = COMPONENTS;
 
 if (defined(__NAMESPACE__ . '\CSP')) {
