08da0d4d2afab3a0290058a1662293bce4b9d262
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-25 18:41:09 -0800

    Do not append nodes other than DOMElements
    If `$node` does not have `tagName` property, continue the for loop.
    
    Remove the `try` / `catch`

--- components/sections/home.php
+++ components/sections/home.php
@@ -14,35 +14,29 @@ return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page, 
 		if (empty($section)) {
 			continue;
 		}
-		try {
-			foreach ($section_template->childNodes as $node) {
-				$node = $main->appendChild($node->cloneNode(true));
-				$node->id = $name;
-				$node->class = 'category';
-			}
 
-			\shgysk8zer0\Core\Console::log([$name => $section]);
-			$container = $main->getElementById($name);
-			$title = $xpath->query('.//h2', $container)->item(0);
-			$link = $title->appendChild($dom->createElement('a'));
-			$title->class = 'center';
-			$link->href = \KVSun\DOMAIN . "{$name}/";
-			$link->textContent = $section[0]->category;
-
-			foreach ($section as $article) {
-				$div = $container->appendChild($dom->createElement('div'));
-				$a = $div->appendChild($dom->createElement('a'));
-				$a->href = \KVSun\DOMAIN . "{$article->catURL}/{$article->url}";
-				$a->textContent = $article->title;
-				$a->class = 'currentColor';
+		foreach ($section_template->childNodes as $name => $node) {
+			if (isset($node->tagName)) {
+				$container = $main->appendChild($node->cloneNode(true));
+				$container->id = $name;
+				$container->class = 'category';
+			} else {
+				continue;
 			}
-		} catch (\Throwable $e) {
-			\shgysk8zer0\Core\Console::error([
-				'message' => $e->getMessage(),
-				'file' => $e->getFile(),
-				'line' => $e->getLine(),
-				'trace' => $e->getTrace(),
-			]);
+		}
+
+		$title = $xpath->query('.//h2', $container)->item(0);
+		$link = $title->appendChild($dom->createElement('a'));
+		$title->class = 'center';
+		$link->href = \KVSun\DOMAIN . "{$name}/";
+		$link->textContent = $section[0]->category;
+
+		foreach ($section as $article) {
+			$div = $container->appendChild($dom->createElement('div'));
+			$a = $div->appendChild($dom->createElement('a'));
+			$a->href = \KVSun\DOMAIN . "{$article->catURL}/{$article->url}";
+			$a->textContent = $article->title;
+			$a->class = 'currentColor';
 		}
 	}
 };
