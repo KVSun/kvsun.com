6aacfb9f084949b88636c94f1b9046a8e9ac26d4
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-25 19:36:42 -0800

    Add site title to page header

--- components/header.php
+++ components/header.php
@@ -1,6 +1,7 @@
 <?php
 namespace KVSun\Components\Header;
-return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page)
+return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page, $kvs)
 {
-	$dom->body->append('header');
+	$dom->body->append('header')->append('h1', $kvs->getHead()->title, ['class' => 'site-title']);
+
 };
