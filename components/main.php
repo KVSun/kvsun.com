101f94d216f25e5bf4ec81faa5ce684dbba035a7
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-25 17:38:32 -0800

    Move sections for templates into own directory

--- components/main.php
+++ components/main.php
@@ -3,5 +3,5 @@ namespace KVSun\Components\Main;
 return function (\shgysk8zer0\DOM\HTML $dom, \shgysk8zer0\Core\PDO $pdo, $page, $kvs)
 {
 	$main = $dom->body->append('main');
-	\KVSun\load($kvs::TYPE);
+	\KVSun\load('sections' . DIRECTORY_SEPARATOR . $kvs::TYPE);
 };
