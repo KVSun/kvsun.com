2f56457ccb97b2454334885532a08e7ffd40ab29
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-14 20:46:06 -0800

    Update image test to create full page
    With same structure & style as any regular page.

--- img.php
+++ img.php
@@ -1,35 +1,88 @@
 <?php
 
-namespace KVSun;
+namespace KVSun\ImageTest;
 
 use \shgysk8zer0\Core as Core;
+use \shgysk8zer0\DOM as DOM;
 
 if (in_array(PHP_SAPI, ['cli', 'cli-server'])) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 }
 
-set_error_handler('shgysk8zer0\Core\Console::error', E_ALL);
-set_exception_handler('shgysk8zer0\Core\Console::error');
+function make_picture(
+	Array           $imgs,
+	DOM\HTMLElement $parent,
+	String          $by      = null,
+	String          $caption = null
+): DOM\HTMLElement
+{
+	$figure = $parent->append('figure');
+	$picture = $figure->append('picture');
+	if (isset($by)) {
+		$figure->append('cite', $by);
+	}
+	if (isset($caption)) {
+		$cap = $figure->append('figcaption', $caption);
+	}
+	foreach($imgs as $format => $img) {
+		$source = $picture->append('source');
+		$source->type = $format;
+		$source->srcset = join(',', array_map(function(Array $src) use ($img): String
+		{
+			return "{$src['path']} {$src['width']}w";
+		}, $img));
+	}
+	$picture->append('img', null, [
+		'src'    => $imgs['image/jpeg'][0]['path'],
+		'width'  => $imgs['image/jpeg'][0]['width'],
+		'height' => $imgs['image/jpeg'][0]['height']
+	]);
+	return $picture;
+}
+
+DOM\HTMLElement::$import_path = \KVSun\COMPONENTS;
+$dom = DOM\HTML::getInstance();
+\KVSun\add_main_menu($dom->body);
+$dom->body->class = 'flex row wrap';
+$dom->head->append('title', __NAMESPACE__);
+
+$dom->head->append('link', null, [
+	'rel' => 'stylesheet',
+	'type' => 'text/css',
+	'href' => \KVSun\DOMAIN . \KVSun\DEV_STYLE,
+]);
+\KVSun\load('header', 'nav');
+$main = $dom->body->append('main', null, [
+	'role' => 'main',
+]);
+if (\KVSun\check_role('editor')) {
+	$main->contextmenu = 'admin_menu';
+}
+\KVSun\load('sidebar', 'footer');
 
 if (array_key_exists('upload', $_FILES)) {
 	$imgs = Core\Image::responsiveImagesFromUpload(
 		'upload',
-		['images', 'uploads', date('Y'), date('m')]
+		['images', 'uploads', date('Y'), date('m')],
+		[1200, 800, 600, 300],
+		['image/webp', 'image/jpeg']
 	);
 	Core\Console::info($imgs);
+	make_picture($imgs, $main, 'Chris Zuber', 'Testing 1 2 3');
 }
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Upload test</title>
-		<meta charset="utf-8" />
-	</head>
-	<body>
-		<form action="<?="{$_SERVER['PHP_SELF']}"?>" method="post" enctype="multipart/form-data">
-			<input type="file" name="upload[]" accept="image/jpeg,image/png,image/gif" multiple required autofocus />
-			<br />
-			<button type="submit">Upload</button>
-		</form>
-	</body>
-</html>
+$form = $main->append('form', null, [
+	'action'  => $_SERVER['PHP_SELF'],
+	'method'  => 'post',
+	'enctype' => 'multipart/form-data',
+], [
+	['input', null, [
+		'type'      => 'file',
+		'name'      => 'upload[]',
+		'accept'    => join(',', array_keys(Core\Image::EXTS)),
+		'required'  => null,
+		'autofocus' => null,
+	]],
+	['br'],
+	['button', 'Upload', ['type' => 'submit']]
+]);
+exit($dom);
