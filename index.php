9cc70a5236387067f2816341cef35aaa36e87ca5
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-21 13:45:38 -0800

    Use KVSun\defined instead of native defined

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ namespace KVSun\Index;
 use const \KVSun\{CSP as CSP_POLICY, DEBUG};
 
 use function \KVSun\{build_dom, get_path};
+use function \KVSun\{defined};
 
 use \shgysk8zer0\Core\CSP;
 use \shgysk8zer0\DOM;
@@ -11,7 +12,7 @@ use \shgysk8zer0\DOM;
 if (in_array(PHP_SAPI, ['cli', 'cli-server'])) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 }
-if (defined('KVSun\CSP')) {
+if (defined('CSP')) {
 	(new CSP(CSP_POLICY))(DEBUG);
 }
 
