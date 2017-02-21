c6bd3f3905ce1db4a3515224d2cae0eaad9cd0b9
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-21 11:17:25 -0800

    Improve aliasing and change namespace of `index.php`

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
-namespace KVSun;
+namespace KVSun\Index;
 
-use \shgysk8zer0\Core;
+use const \KVSun\{CSP as CSP_POLICY, DEBUG};
+
+use function \KVSun\{build_dom, get_path};
+
+use \shgysk8zer0\Core\CSP;
 use \shgysk8zer0\DOM;
 
 if (in_array(PHP_SAPI, ['cli', 'cli-server'])) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 }
 if (defined('KVSun\CSP')) {
-	(new Core\CSP(\KVSun\CSP))(\KVSun\DEBUG);
+	(new CSP(CSP_POLICY))(DEBUG);
 }
 
 exit(build_dom(get_path())->saveHTML());
