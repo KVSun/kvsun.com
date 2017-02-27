ab8d9285f275b23cc5a26919a75d6844e3e58dc2
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-26 16:11:59 -0800

    Convert email to lower case on installation and registration

--- components/handlers/form.php
+++ components/handlers/form.php
@@ -124,7 +124,7 @@ switch($req->form) {
 					);'
 				);
 
-				$user->email = $install->user->email;
+				$user->email = strtolower($install->user->email);
 				$user->username = $install->user->username;
 				$user->password = password_hash($install->user->password, PASSWORD_DEFAULT);
 				$user->execute();
@@ -322,9 +322,9 @@ switch($req->form) {
 					NULL
 				);');
 				$users->execute([
-					'email' => $req->register->email,
-					'username' => $req->register->username,
-					'password' => password_hash($req->register->password, \PASSWORD_DEFAULT)
+					'email'    => strtolower($req->register->email),
+					'username' => strtolower($req->register->username),
+					'password' => password_hash($req->register->password, PASSWORD_DEFAULT)
 				]);
 				$user_data->execute(['name' => $req->register->name]);
 				$subscribers->execute(['status' => get_role_id('guest')]);
