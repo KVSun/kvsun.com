4bf88d0fd8932998133ba7e446d729e6ea7b0bec
Chris Zuber <shgysk8zer0@gmail.com>
2017-03-09 23:13:50 -0800

    Correctly set Content-Security-Policy
    Skip checking if CSP is defined. It is!

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ use \shgysk8zer0\Core\{CSP};
 if (in_array(PHP_SAPI, ['cli', 'cli-server'])) {
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
 }
-if (defined('CSP_POLICY')) {
-	(new CSP(CSP_POLICY))();
-}
+
+(new CSP(CSP_POLICY))();
 
 exit(build_dom(get_path())->saveHTML());
