736a14e9521168520e8a246b59f608c257506e39
Chris Zuber <shgysk8zer0@gmail.com>
2017-03-17 13:05:57 -0700

    Use report-uri.io instead of own CSP report URI

--- csp.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace KVSun\CSP;
-use \shgysk8zer0\Core\{Headers, Console};
-$headers = new Headers();
-if (in_array($headers->content_type, ['application/json', 'application/csp-report'])) {
-	$report = json_decode(file_get_contents('php://input'));
-	Console::log($report);
-}
