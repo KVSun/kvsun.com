5cbb68a7d7165a747be7b2b5c4d5e4489812ec3f
Chris Zuber <shgysk8zer0@gmail.com>
2017-03-16 17:33:47 -0700

    Add button to toggle visibility of nav on mobile
    Resolves #110
    
    Toggles `menu-open` class using `data-toggle-class` attribute

--- components/header.php
+++ components/header.php
@@ -3,13 +3,21 @@ namespace KVSun\Components\Header;
 
 use \shgysk8zer0\DOM\{HTML};
 use \shgysk8zer0\Core\{PDO};
-use \KVsun\KVSAPI\{Abstracts\Content as KVSAPI};
+use \KVSun\KVSAPI\{Abstracts\Content as KVSAPI};
+use function \KVSun\Functions\{use_icon};
 
 return function (HTML $dom, PDO $pdo, KVSAPI $kvs)
 {
-	$dom->body->append('header')->append('h1', $kvs->getHead()->title, [
+	$header = $dom->body->append('header')->append('h1', $kvs->getHead()->title, [
 		'class' => 'site-title',
 		'role'  => 'banner',
 	]);
-
+	$mobile_toggle = $header->append('button', null, [
+		'data-toggle-class' => '{"#main-nav": "menu-open"}',
+		'class'             => 'mobile-only',
+		'id'                => 'mobile-menu-btn',
+	]);
+	use_icon('hamburger', $mobile_toggle, [
+		'class' => 'icon'
+	]);
 };
